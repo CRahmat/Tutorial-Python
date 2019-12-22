0d2d8d438c0283087d419dccc113f8498a0dbb10
CaturRahmat <UnclearAccount@users.github.com>
2019-12-22 11:02:03 +0700

    Add String Operation

---
+++ String/string.py
@@ -0,0 +1,27 @@
+#print string
+print('Hello Bro!!!!')
+#print with '
+print('Hari ini hari Jum\'at')
+print("Hari ini hari Jum'at")
+#print with "
+print('Selamat Siang, "Apa Kabar?"')
+print("Selamat Siang, \'Apa Kabar?\'")
+#print multi line
+print("""
+Saya = "Selamat Siang?"
+Budi = "Selamat Siang, Apa Kabar?"
+Saya = "Baik, Bagaimana dengan Anda?"
+Budi = "Baik juga"
+""")
+#print directory (raw String)
+#Jika tidak di tambahkan huruf r maka akan dibaca enter
+print(r'C:\user\caturrahmat\Downloads\Documents')
+
+#print string array
+nama = "Catur Rahmat"
+printNama = nama[0:13]
+print("Nama Saya adalah : ",printNama)
+printNama2 = nama[-10:0]
+print("Nama Saya adalah : ",printNama2)
+
+#Sekian
